89d2e8b09ba36106b77f1de260659abd2409f10a
Rizwan <akramrizwan8@gmail.com>
2018-08-07 02:12:07 +0530

    Some Css Changed

--- src/components/ImageLinkForm/ImagelinkForm.js
+++ src/components/ImageLinkForm/ImagelinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmmit}) => {
      	{'This Magic Brain will detect your face picture, Get a Try:'}
      </p>
       <div className ='center'  >
-        <div className='center form pa2 br3 shadow-5'>
+        <div className='center form pa4 br3 shadow-5'>
      	<input type='text' className = 'f3 pa4 w-70 center' onChange = {onInputChange}/>
      	<button
      	 className = ' w-30 grow f4 link ph3 pv2 dib white bg-blue'
